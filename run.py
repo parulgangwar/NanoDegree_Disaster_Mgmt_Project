411a90ce3eed75dc1da9abe2844a59dac0f0d699
parulgangwar <81389574+parulgangwar@users.noreply.github.com>
2024-01-14 17:37:19 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('DisasterResponse', con=engine)
+df = pd.read_sql_table('disaster_messages', con=engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
